4d6d82c1edf04ded5f591178fc39c3bb1c6898bd
Umedzhon Abdumuminov <umed@uber.com>
2024-11-05 17:06:35 +0100

    Add keymappings

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -5,6 +5,12 @@
 ---@type ChadrcConfig
 local M = {}
 
+vim.cmd("highlight St_relativepath guifg=#626a83 guibg=#2a2b36")
+
+local stbufnr = function()
+  return vim.api.nvim_win_get_buf(vim.g.statusline_winid or 0)
+end
+
 M.base46 = {
 	theme = "onedark",
 	hl_override = {
@@ -16,6 +22,21 @@ M.base46 = {
 M.ui= {
   tabufline= {
   order = { "treeOffset" }
+  },
+  statusline = {
+    theme = "default",
+    order = { "mode", "relativepath", "file", "git", "%=", "lsp_msg", "%=", "diagnostics", "lsp", "cwd", "cursor" },
+    modules = {
+      relativepath = function()
+        local path = vim.api.nvim_buf_get_name(stbufnr())
+
+        if path == "" then
+          return ""
+        end
+
+        return "%#St_relativepath#  " .. vim.fn.expand("%:.:h") .. " /"
+      end,
+    },
   },
 }
 
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,6 +1,19 @@
 require "nvchad.mappings"
 
 -- add yours here
+vim.api.nvim_create_user_command("CopyFullPath", function()
+    local path = vim.fn.expand("%:p")
+    vim.fn.setreg("+", path)
+    vim.notify('Copied "' .. path .. '" to the clipboard!')
+end, {})
+
+vim.api.nvim_create_user_command("CopyRelPath",
+  function()
+    vim.api.nvim_call_function(
+      "setreg",
+      {"+", vim.fn.fnamemodify(vim.fn.expand("%"), ":.")})
+  end,
+  {})
 
 local map = vim.keymap.set
 
@@ -8,5 +21,7 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map("n", "<leader>fs", "<CMD>Telescope grep_string<CR>", { desc = "search for word under cursor"})
 map("x", "<leader>fs", "<CMD>Telescope grep_string<CR>", { desc = "search for selected text"})
+map("n", "cp", "<CMD>CopyRelPath<CR>", { desc = "copy relative file path" })
+map("n", "cfp", "<CMD>CopyFullPath<CR>", { desc = "copy full file path" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
